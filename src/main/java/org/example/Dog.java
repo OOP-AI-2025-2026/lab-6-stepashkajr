00376e3b5ecbfe60a133b6efa5414bb31c557820
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-12-03 20:27:29 +0100

    completed lab6

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    String makeSound() {
+        return "Гав-гав!";
+    }
+}
