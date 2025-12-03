909c56618e796a312aab35a4aeda90dc1bc587fb
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-12-03 20:25:01 +0100

    completed lab6

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    String makeSound() {
+        return "Мяу!";
+    }
+}
